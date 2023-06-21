8494e7de3f132d612db888cd5093ef88455f8590
Arthur Brandão <arthurbrdantas@gmail.com>
2023-06-20 21:53:35 -0300

    feat: major improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-function App() {
+import { api } from './api'
+import { useEffect, useState } from 'react'
+import { User } from './types/user'
+
+const people = [
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristen Ramos',
+    email: 'kristen.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+
+export default function Example() {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data: users } = await api.get('/users')
+      setUsers(users)
+    }
+
+    fetchUsers()
+  }, [])
+
   return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
+    <ul className="divide-y divide-gray-200">
+      {users.map((user) => (
+        <li key={user.email} className="py-4 flex">
+          <div className="ml-3">
+            <p className="text-sm font-medium text-gray-900">{user.name}</p>
+            <p className="text-sm text-gray-500">{user.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
-
-export default App
---
+++ src/types/user.ts
@@ -0,0 +1,14 @@
+export type User = {
+  id?: number
+  isActive?: boolean
+  createdAt?: string
+  lastUpdated?: string
+  name: string
+  login: string
+  password: string
+  email: string
+  phone: string
+  cpf: string
+  birthdate: string
+  motherName: string
+}
